ad1ff0f755caa4b7baf5126616b943c4dc446546
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2024-04-27 18:21:37 +0900

    fix cuda12 install script

--- script/install_cuda12.4_ubuntu20.sh
+++ script/install_cuda12.4_ubuntu20.sh
@@ -8,7 +8,7 @@
 #   cudnn: https://developer.nvidia.com/rdp/cudnn-download
 #   tips: https://qiita.com/Manyan3/items/628b3b22700fd569e8fb
 ###
-set -e
+# to ignore "The public cudnn-local-repo-debian12-9.1.0 GPG key does not appear to be installed." error, do not; set -e.
 
 echo "[INFO] remove existing cuda to avoid compatible issues..."
 sudo apt remove --purge "nvidia-*" -y && sudo apt autoremove -y
@@ -19,39 +19,44 @@ echo "[INFO] done removal."
 
 echo "[INFO] update cuda GPG key..."
 sudo apt-key del 7fa2af80
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+DEB_FILE=cuda-keyring_1.0-1_all.deb
+if [ ! -f ${DEB_FILE} ]; then
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+fi
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 echo "[INFO] done updating GPG key."
 
-echo "[INFO] install base..."
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.4.1/local_installers/cuda-repo-ubuntu2004-12-4-local_12.4.1-550.54.15-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-12-4-local_12.4.1-550.54.15-1_amd64.deb
+echo "[INFO] install driver and cudnn..."
+PIN_FILE=cuda-ubuntu2004.pin
+if [ ! -f ${PIN_FILE} ]; then
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+fi
+sudo mv ${PIN_FILE} /etc/apt/preferences.d/cuda-repository-pin-600
+DEB_FILE=cuda-repo-ubuntu2004-12-4-local_12.4.1-550.54.15-1_amd64.deb
+if [ ! -f ${DEB_FILE} ]; then
+    wget https://developer.download.nvidia.com/compute/cuda/12.4.1/local_installers/cuda-repo-ubuntu2004-12-4-local_12.4.1-550.54.15-1_amd64.deb
+fi
+sudo dpkg -i ${DEB_FILE}
 sudo cp /var/cuda-repo-ubuntu2004-12-4-local/cuda-*-keyring.gpg /usr/share/keyrings/
+DEB_FILE=cudnn-local-repo-debian12-9.1.0_1.0-1_amd64.deb
+if [ ! -f ${DEB_FILE} ]; then
+    wget https://developer.download.nvidia.com/compute/cudnn/9.1.0/local_installers/cudnn-local-repo-debian12-9.1.0_1.0-1_amd64.deb
+fi
+sudo dpkg -i ${DEB_FILE}
+sudo cp /var/cudnn-local-repo-ubuntu2004-9.1.0/cudnn-*-keyring.gpg /usr/share/keyrings/
+sudo add-apt-repository contrib
+
 sudo rm -f /etc/apt/sources.list.d/cuda.list
 sudo rm -f /etc/apt/sources.list.d/nvidia-ml.list
 sudo apt-get update
 sudo apt-get -y install cuda-toolkit-12-4
-echo "[INFO] done base installation."
-
-echo "[INFO] install driver..."
-sudo apt-get install -y nvidia-driver-550-open
-sudo apt-get install -y cuda-drivers-550
-echo "[INFO] done installing the driver."
-
-echo "[INFO] install cuDNN..."
-wget https://developer.download.nvidia.com/compute/cudnn/9.1.0/local_installers/cudnn-local-repo-debian12-9.1.0_1.0-1_amd64.deb
-sudo dpkg -i cudnn-local-repo-debian12-9.1.0_1.0-1_amd64.deb
-sudo cp /var/cuda-repo-debian12-9-1-local/cudnn-*-keyring.gpg /usr/share/keyrings/
-sudo add-apt-repository contrib
-sudo apt-get update -y
+sudo apt-get -y install nvidia-driver-550-open
+sudo apt-get -y install cuda-drivers-550
 sudo apt-get -y install cudnn
 sudo apt-get -y install cudnn-cuda-12
-echo "[INFO] done installing cuDNN."
+echo "[INFO] done installations."
 
 echo "[INFO] clean up downloaded files..."
-rm cuda-repo-ubuntu2004-12-4-local_12.4.1-550.54.15-1_amd64.deb
-rm cudnn-local-repo-debian12-9.1.0_1.0-1_amd64.deb
+rm -f *.deb*
 echo "[INFO] done. please reboot."
 
